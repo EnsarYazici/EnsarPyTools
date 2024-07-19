6b920399613f23ff79ea8d38e3cca5c26b13e1a0
EnsarYazici <65979531+EnsarYazici@users.noreply.github.com>
2024-07-19 16:24:50 +0300

    Update SelfKiller_Exe.py

--- SelfKiller/SelfKiller_Exe.py
+++ SelfKiller/SelfKiller_Exe.py
@@ -1,30 +1,28 @@
-# For döngüsü ile 0'dan 100'e kadar sayma
-for i in range(6):
-    print(str(5-i)+"  ",end="\r")
-    time.sleep(1)  # Sayıları yavaşça göstermek için
+def CheckSelf(RedFlag = False,exeName = "main", batchCoolDown = 1):
+    import time
+    import os
 
-# Sayı 100 olduğunda batch dosyasını oluştur
-if i == 5:
-    batch_content = f"""@echo off
-start /B
+    if RedFlag:
+        batch_content = f"""@echo off
+    start /B
 
-set EXE_FILE=xxx.exe
+    set EXE_FILE={exeName}.exe
 
-taskkill /F /IM %EXE_FILE% >nul 2>&1
+    taskkill /F /IM %EXE_FILE% >nul 2>&1
 
-timeout /t 1 /nobreak >nul
+    timeout /t 1 /nobreak >nul
 
-if exist %EXE_FILE% (
-    del %EXE_FILE%
+    if exist %EXE_FILE% (
+        del %EXE_FILE%
 
-)
-(del "%~f0")>nul 2>nul
-exit
-    """
-    batch_file_path = "delete_files.bat"
-    with open(batch_file_path, "w") as batch_file:
-        batch_file.write(batch_content)
+    )
+    (del "%~f0")>nul 2>nul
+    exit
+        """
+        batch_file_path = "goodbye.bat"
+        with open(batch_file_path, "w") as batch_file:
+            batch_file.write(batch_content)
 
-    time.sleep(1)
-    #Batch dosyasını çalıştır
-    os.system(f"start /b {batch_file_path}")
+        time.sleep(batchCoolDown)
+        #Batch dosyasını çalıştır
+        os.system(f"start /b {batch_file_path}")
