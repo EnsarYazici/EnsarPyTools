9f3732c39b3744167a9e8954c102c22fb4af92e8
EnsarYazici <65979531+EnsarYazici@users.noreply.github.com>
2024-07-19 18:01:02 +0300

    Update EYRegexFuncs.py
    target parametresi eklendi

--- Regex/EYRegexFuncs.py
+++ Regex/EYRegexFuncs.py
@@ -1,12 +1,13 @@
-def Regex_AtoB(Text, A, B, name = "word" ,incA = False, incB = False,ignorecase = False,returnList = False):
-
-    pattern = fr'{A}(.*?){B}'
-
+def Regex_AtoB(Text, A, B, name = "word" ,incA = False, incB = False,ignorecase = False, target = "(.*?)", returnList = False):
+    # For Name target -> ([a-zA-ZğüşıöçĞÜŞİÖÇ\s-]+)
+    # For Everything target -> (.*?)
+    pattern = fr'{A}{target}{B}'
+    
     flags = re.IGNORECASE if ignorecase else 0
-
+    
     if returnList:
         matches = re.findall(pattern, Text, flags)
-
+    
         if matches:
             results = []
             for match in matches:
@@ -16,21 +17,21 @@ def Regex_AtoB(Text, A, B, name = "word" ,incA = False, incB = False,ignorecase 
                 if incB:
                     result = result + B
                 results.append(result.strip())
-
+    
             return results
         else:
             print(f"Regex_AtoB -> {name} is Not Matched")
             return ["Tanimlanamadi"]
     else:
         match = re.search(pattern, Text,flags)
-
+    
         if match:
             result = match.group(1)
             if incA:
                 result = A + result
             if incB:
                 result = result + B
-
+    
             return result.strip()
         else:
             print(f"Regex_AtoB -> {name} is Not Matched")
