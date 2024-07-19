9a2fc820f48f807cea4b5990795a144d1e47d40e
EnsarYazici <65979531+EnsarYazici@users.noreply.github.com>
2024-07-19 17:13:48 +0300

    Update EYStringBender.py

--- Strings/EYStringBender.py
+++ Strings/EYStringBender.py
@@ -1,12 +1,8 @@
-def GetWords(metin,
-             target=(0, 3),
-             defaultEmptyValue="Tanimlanamadi",
-             customSeperator=None,
-             unifier=" "):
-    kelimeler = metin.split(
-        customSeperator) if customSeperator else metin.split()
-    print(kelimeler)
-    if len(kelimeler) == 0:
+def GetWords(text, target=(0, 3), defaultEmptyValue="Tanimlanamadi", customSeperator=None, unifier=" "):
+    words = text.split(
+        customSeperator) if customSeperator else text.split()
+    # print(words)
+    if len(words) == 0:
         return defaultEmptyValue
     else:
-        return unifier.join(kelimeler[target[0]:target[1]])
+        return unifier.join(words[target[0]:target[1]])
