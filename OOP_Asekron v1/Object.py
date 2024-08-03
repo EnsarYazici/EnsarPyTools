14762da4e502473fa8d878ddd3e1c044bfdcfe27
EnsarYazici <65979531+EnsarYazici@users.noreply.github.com>
2024-08-03 23:35:58 +0300

    Update Object.py

--- OOP_Asekron v1/Object.py
+++ OOP_Asekron v1/Object.py
@@ -1,19 +1,21 @@
 import asyncio
 
 class ObjectBase:
-    def __init__(self, update_interval=1.0):
+    def __init__(self,id = -1, update_interval=1.0):
         self.update_interval = update_interval
         self.running = True
+        self.id = id
 
-    async def start(self):
-        await self.on_start()
+    async def run(self):
+        await self.start()
         if hasattr(self, 'update'):
             while self.running:
                 await self.update()
                 await asyncio.sleep(self.update_interval)
 
-    async def on_start(self):
+    async def start(self):
         pass  # Subclasses can override this method
 
     def stop(self):
         self.running = False
+        print(f"Object{self.id} sonlandirildi")
