f7c65f36bbe5f32c9d117cc5dc91ae2a4b4779d5
EnsarYazici <65979531+EnsarYazici@users.noreply.github.com>
2024-08-03 20:27:27 +0300

    Update Object.py
    adding stop method

--- OOP_Asekron v1/Object.py
+++ OOP_Asekron v1/Object.py
@@ -3,13 +3,17 @@ import asyncio
 class ObjectBase:
     def __init__(self, update_interval=1.0):
         self.update_interval = update_interval
+        self.running = True
 
     async def start(self):
         await self.on_start()
         if hasattr(self, 'update'):
-            while True:
+            while self.running:
                 await self.update()
                 await asyncio.sleep(self.update_interval)
 
     async def on_start(self):
         pass  # Subclasses can override this method
+
+    def stop(self)
+        self.running = False
