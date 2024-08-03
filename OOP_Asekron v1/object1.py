59d967bd6fb785db88ff5a06c8ad84d42e1a7f02
EnsarYazici <65979531+EnsarYazici@users.noreply.github.com>
2024-08-03 23:38:32 +0300

    Update object1.py

--- OOP_Asekron v1/object1.py
+++ OOP_Asekron v1/object1.py
@@ -1,14 +1,19 @@
 from Object import ObjectBase
 import asyncio
+
 class Object1(ObjectBase):
     def __init__(self,id = -1,update_inerval = 1):
         super().__init__(update_inerval)
         self.id = id
-    async def on_start(self):
+        self.a = 0
+    async def start(self):
         print(f"Object{self.id} start")
 
     async def update(self):
-        print("Object1 update")
+        print(f"Object{self.id} update")
         for i in range(5):
             print(f"Object{self.id} processing {i}")
             await asyncio.sleep(1)  # Simulate a long-running task
+            self.a += 1
+        if self.a >= 10:
+          self.stop()
