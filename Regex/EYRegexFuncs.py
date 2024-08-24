b7af517ab569f4336a67bda00cbf370cd714967c
KaldrmMuhendisi <65979531+EnsarYazici@users.noreply.github.com>
2024-08-24 21:32:21 +0300

    Update EYRegexFuncs.py
    Dotall eklendi
    
    amacı:
    A
    hedef
    B
    
    noramlde almıyordu ama dotal flagı ekelnince hedefi alııyor

--- Regex/EYRegexFuncs.py
+++ Regex/EYRegexFuncs.py
@@ -1,13 +1,17 @@
-def Regex_AtoB(Text, A, B, name = "word" ,incA = False, incB = False,ignorecase = False, target = "(.*?)", returnList = False):
-    # For Name target -> ([a-zA-ZğüşıöçĞÜŞİÖÇ\s]+) if Names have - or . use ([a-zA-ZğüşıöçĞÜŞİÖÇ\s.-]+)
+def Regex_AtoB(Text, A, B, name="word", incA=False, incB=False, ignorecase=False, dotall=False, target="(.*?)", returnList=False):
+    # For Name target -> ([a-zA-ZğüşıöçĞÜŞİÖÇ\s-]+)
     # For Everything target -> (.*?)
     pattern = fr'{A}{target}{B}'
-    
-    flags = re.IGNORECASE if ignorecase else 0
-    
+
+    flags = 0
+    if ignorecase:
+        flags |= re.IGNORECASE
+    if dotall:
+        flags |= re.DOTALL
+
     if returnList:
         matches = re.findall(pattern, Text, flags)
-    
+
         if matches:
             results = []
             for match in matches:
@@ -17,21 +21,21 @@ def Regex_AtoB(Text, A, B, name = "word" ,incA = False, incB = False,ignorecase 
                 if incB:
                     result = result + B
                 results.append(result.strip())
-    
+
             return results
         else:
             print(f"Regex_AtoB -> {name} is Not Matched")
             return ["Tanimlanamadi"]
     else:
-        match = re.search(pattern, Text,flags)
-    
+        match = re.search(pattern, Text, flags)
+
         if match:
             result = match.group(1)
             if incA:
                 result = A + result
             if incB:
                 result = result + B
-    
+
             return result.strip()
         else:
             print(f"Regex_AtoB -> {name} is Not Matched")
