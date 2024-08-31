f41ca4f1d0794080a183d460791f19c33b72cfde
KaldrmMuhendisi <65979531+EnsarYazici@users.noreply.github.com>
2024-08-31 17:11:33 +0300

    Update EYRegexFuncs.py -> outlook için doğrulama kodu eklendi

--- Regex/EYRegexFuncs.py
+++ Regex/EYRegexFuncs.py
@@ -40,3 +40,54 @@ def Regex_AtoB(Text, A, B, name="word", incA=False, incB=False, ignorecase=False
         else:
             print(f"Regex_AtoB -> {name} is Not Matched")
             return "Tanimlanamadi"
+
+
+def DogrulamaKoduAl_Oulook(metin):
+    result = ""
+    pat = "(?:(?:\d{1,2})?\s*[-.])?\s*([A-Z0-9]{20})"
+    patfull = ""
+    for i in range(7):
+        patfull += f"\s*(?:{pat})?"
+
+    # print(patfull)
+
+    # \s*(?:{pat})?\s*(?:{pat})?\s*(?:{pat})?\s*(?:{pat})?\s*(?:{pat})?\s*(?:{pat})?\s*(?:{pat})?"
+
+    try:
+        pattern = fr"Ek\s*(?:.*)?\s*Do[gĞ]rulama\s*Kod[u]?\s*[:]{patfull}"
+        DogrulamaKodu = re.findall(pattern, metin,re.IGNORECASE)
+        if DogrulamaKodu:
+            kod = ""
+            for i in range(len(DogrulamaKodu[0])):
+                if DogrulamaKodu[0][(len(DogrulamaKodu[0])-1)-i] != "Not" and DogrulamaKodu[0][(len(DogrulamaKodu[0])-1)-i] != "":
+                    kod = DogrulamaKodu[0][(len(DogrulamaKodu[0])-1)-i]
+                    break
+            if kod != "":
+                print(f"Ek Karar -> {kod}")
+                result = kod
+            else:
+                print("else1")
+                pattern = r"Do[gĞ]rulama\s*Kod[u]?\s*[:]\s*(?:\d-)?\s*([A-Za-z0-9]+)"
+                DogrulamaKodu = re.findall(pattern, metin,re.IGNORECASE)
+                if DogrulamaKodu:
+                    print(f"Doğrulama -> {DogrulamaKodu[0]}")
+                    result = DogrulamaKodu[0]
+        else:
+            print("else2")
+            pattern = r"Do[gĞ]rulama\s*Kod[u]?\s*[:]\s*(?:\d-)?\s*([A-Za-z0-9]+)"
+            DogrulamaKodu = re.findall(pattern, metin,re.IGNORECASE)
+            if DogrulamaKodu:
+                print(f"Doğrulama -> {DogrulamaKodu[0]}")
+                result = DogrulamaKodu[0]
+
+        def check_string(s):
+            pattern = r'^[A-Z0-9]{20}$'
+            return re.match(pattern, s) is not None
+
+        if check_string(result):
+            return result
+        else:
+            print("else3")
+            return "Tanimlanamadi"
+    except:
+        return "Tanimlanamadi"
